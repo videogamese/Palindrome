30a5e2bd6df51c5657b1ad4072156eab109f1287
fy48 <fy48@fresnoU.org>
2023-09-06 08:39:39 -0700

    started 3.3

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {
+    phrase: req.body.userText,
+    message: getResultDescription(req.body.userText)
+  } )
+});
+
+router.post('/', function ())
+function checkPalindrome(phrase){
+  let temp = phrase.split("");
+  temp = temp.reverse();
+  temp = temp.join("")
+  if (phrase.toLowerCase().replace(" ", "") === temp.toLowerCase().replace(" ", "") ){
+    return true
+  }
+  else {
+    return false
+  }
+}
+
+function getResultDescription (phrase){
+  if (checkPalindrome(phrase)) {
+    return `${phrase} is a palindrome.`
+  }
+  return `${phrase} is not a palindrome.`
+}
+module.exports = router;
